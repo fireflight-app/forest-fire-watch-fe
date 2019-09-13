54f05f5139038731971b46647f4da6814d430137
duckets615 <chris.reilly024@gmail.com>
2019-09-13 18:15:43 -0400

    setup

---
+++ fireflight/src/components/AddressModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const AddressModal = (props) => (
+  <Modal
+  isOpen={true}
+  contentLabel='address input'
+  >
+  <h3>selected option</h3>
+  </Modal>
+)
+
+export default AddressModal;
